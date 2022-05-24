b7baf56cbea0799539ad85d6724c24c141cd2a8d
szalanski <szalanski91@gmail.com>
2022-05-24 21:44:24 +0200

    Add mining block function

--- rust-chain/src/core/block.rs
+++ rust-chain/src/core/block.rs
@@ -1,5 +1,10 @@
+use chrono::Utc;
+use std::collections::hash_map::DefaultHasher;
+use std::hash::Hash;
+use std::hash::Hasher;
+
 #[derive(Debug)]
-pub struct Block<T: Default> {
+pub struct Block<T: Default + Hash> {
     timestamp: i64,
     lastHash: u64,
     hash: u64,
@@ -8,7 +13,7 @@ pub struct Block<T: Default> {
 
 impl<T> Block<T>
 where
-    T: Default,
+    T: Default + Hash,
 {
     pub fn new(timestamp: i64, lastHash: u64, hash: u64, data: T) -> Block<T> {
         Block {
@@ -26,5 +31,18 @@ where
             hash: 12076202582256590650,
             data: T::default(),
         }
+    }
+
+    pub fn mine_block(lastBlock: Block<T>, data: T) -> Block<T> {
+        let timestamp = Utc::now().timestamp_millis();
+        let lastHash = lastBlock.hash;
+        let hash = Block::generate_hash(&data);
+        Block::new(timestamp, lastHash, hash, data)
+    }
+
+    fn generate_hash(data: &T) -> u64 {
+        let mut hasher = DefaultHasher::new();
+        data.hash(&mut hasher);
+        hasher.finish()
     }
 }
--- rust-chain/src/main.rs
+++ rust-chain/src/main.rs
@@ -4,22 +4,21 @@ use std::collections::hash_map::DefaultHasher;
 use std::hash::Hash;
 use std::hash::Hasher;
 
-#[derive(Debug)]
-struct BlockData;
-impl Default for BlockData {
-    fn default() -> Self {
-        Self {}
-    }
-}
+#[derive(Debug, Hash, Default)]
+struct Data;
 
 fn main() {
+    let genesis: Block<Data> = Block::genesis();
+    println!("Genesis block for rust-blockchain: {:#?}", genesis);
+    let minedBlock = Block::mine_block(genesis, Data);
+    println!("First mined block: {:#?}", minedBlock);
+}
+
+fn new_block_example() {
     let mut value = 128;
     let mut hasher = DefaultHasher::new();
     value.hash(&mut hasher);
 
     let firstBlock = Block::new(Utc::now().timestamp_millis(), 0, hasher.finish(), value);
     println!("First block of rust-blockchain: {:#?}", firstBlock);
-
-    let genesis: Block<BlockData> = Block::genesis();
-    println!("Genesis block for rust-blockchain: {:#?}", genesis);
 }
