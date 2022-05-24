0076ac452f315e754bb5b56ab7aeadcccb9e0a84
szalanski <szalanski91@gmail.com>
2022-05-24 20:50:47 +0200

    Implement genesis block

--- rust-chain/src/core/block.rs
+++ rust-chain/src/core/block.rs
@@ -1,18 +1,30 @@
 #[derive(Debug)]
-pub struct Block<T> {
+pub struct Block<T: Default> {
     timestamp: i64,
     lastHash: u64,
     hash: u64,
     data: T,
 }
 
-impl<T> Block<T> {
-    pub const fn new(timestamp: i64, lastHash: u64, hash: u64, data: T) -> Block<T> {
+impl<T> Block<T>
+where
+    T: Default,
+{
+    pub fn new(timestamp: i64, lastHash: u64, hash: u64, data: T) -> Block<T> {
         Block {
             timestamp,
             hash,
             lastHash,
             data,
+        }
+    }
+
+    pub fn genesis() -> Block<T> {
+        Block {
+            timestamp: 0,
+            lastHash: 0,
+            hash: 12076202582256590650,
+            data: T::default(),
         }
     }
 }
--- rust-chain/src/main.rs
+++ rust-chain/src/main.rs
@@ -4,12 +4,22 @@ use std::collections::hash_map::DefaultHasher;
 use std::hash::Hash;
 use std::hash::Hasher;
 
+#[derive(Debug)]
+struct BlockData;
+impl Default for BlockData {
+    fn default() -> Self {
+        Self {}
+    }
+}
+
 fn main() {
     let mut value = 128;
     let mut hasher = DefaultHasher::new();
-
     value.hash(&mut hasher);
-    let firstBlock = Block::new(Utc::now().timestamp_millis(), 0, hasher.finish(), value);
 
+    let firstBlock = Block::new(Utc::now().timestamp_millis(), 0, hasher.finish(), value);
     println!("First block of rust-blockchain: {:#?}", firstBlock);
+
+    let genesis: Block<BlockData> = Block::genesis();
+    println!("Genesis block for rust-blockchain: {:#?}", genesis);
 }
