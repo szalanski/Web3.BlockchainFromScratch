3b20779e06f9a73c0fba8a87839a3255fd9e5d83
szalanski <szalanski91@gmail.com>
2022-06-10 22:03:08 +0200

    Add mine block test

--- rust-chain/src/core/block_tests.rs
+++ rust-chain/src/core/block_tests.rs
@@ -1,8 +1,27 @@
-use super::{block, hash::Hash, hash::HashValue};
-use sha2::Sha256;
+use super::{hash::Hash, hash::HashValue};
+use crate::core::block::Block;
+use sha2::{Digest, Sha256};
+use std::clone;
 
-//Unit tests goes here
-// #[test]
-// fn unit_test() {
-//     assert!(true)
-// }
+///Data type under test.
+#[derive(Debug, Default, Eq, PartialEq, Copy, Clone)]
+struct Data {
+    field: i32,
+}
+
+impl Hash for Data {
+    fn hash(&self) -> HashValue {
+        Sha256::digest(self.field.to_string()).into()
+    }
+}
+
+#[test]
+fn mine_block_generates_second_block_correctly() {
+    let data = Data { field: 10 };
+    let lastBlock: Block<Data> = Block::genesis();
+
+    let block = Block::mine_block(&lastBlock, data.clone());
+
+    assert!(data == block.data);
+    assert!(block.last_hash == lastBlock.hash);
+}
